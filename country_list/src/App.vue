6c8db71af374a131ea087e95729821417fa39012
mike <>
2020-04-15 16:05:49 +0100

    country data atted to fetch within mounted method

--- country_list/src/App.vue
+++ country_list/src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      countries: [],
+      selectedCountry: null
+    };
+  },
+  mounted(){
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then(countries => this.countries = countries)
+
+    eventBus.$on('country-selected', (country) => {
+      this.selectedCountry = country;
+    })
   }
 }
 </script>
---
+++ country_list/src/components/countryDetail.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+  <div v-if="country">
+
+
+  </div>
+
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
